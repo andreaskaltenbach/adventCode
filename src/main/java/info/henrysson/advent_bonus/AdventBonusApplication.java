88d9e2aa565d2d6f78efa7c127cd6de033ae7b3f
Mattias <git@henrysson.info>
2021-01-17 12:11:36 +0100

    Spring Boot initializer

---
+++ src/main/java/info/henrysson/advent_bonus/AdventBonusApplication.java
@@ -0,0 +1,13 @@
+package info.henrysson.advent_bonus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventBonusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventBonusApplication.class, args);
+	}
+
+}

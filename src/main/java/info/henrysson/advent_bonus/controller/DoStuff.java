ca8d7ca65112f350aa4d49c14aaba9b52c28f333
Mattias <git@henrysson.info>
2021-01-19 00:34:43 +0100

    Meh

--- src/main/java/info/henrysson/advent_bonus/controller/DoStuff.java
+++ src/main/java/info/henrysson/advent_bonus/controller/DoStuff.java
@@ -1,10 +1,10 @@
 package info.henrysson.advent_bonus.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import info.henrysson.advent_bonus.model.City;
 import info.henrysson.advent_bonus.model.MatCategory;
-import info.henrysson.advent_bonus.model.MatProduct;
 import info.henrysson.advent_bonus.service.MatCategoryService;
-import info.henrysson.advent_bonus.service.MatProductService;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
@@ -12,31 +12,53 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Component
 @Profile("!test")
 public class DoStuff {
     private final MatCategoryService matCategoryService;
-    private final MatProductService matProductService;
 
     @Autowired
-    public DoStuff(MatCategoryService matCategoryService, MatProductService matProductService) {
+    public DoStuff(MatCategoryService matCategoryService) {
         this.matCategoryService = matCategoryService;
-        this.matProductService = matProductService;
     }
 
     @EventListener(ApplicationReadyEvent.class)
     public void DoTheStuff(ApplicationReadyEvent applicationReadyEvent) throws IOException {
         System.out.println("Doing stuff");
+        ObjectMapper mapper = new ObjectMapper();
         MatCategory m = matCategoryService.getCategoryTree(City.GOTHENBURG);
-        List<MatProduct> p = matProductService.getCategoryProducts(6830);
-        List<MatProduct> mp = m.getProducts();
-        System.out.println(p.size());
-        System.out.println(mp.size());
-        for (MatCategory mc : m.getSubCategories()) {
-            System.out.println(mc.getProducts().size());
-        }
+        Files.writeString(Paths.get("target/productCount.json"), mapper.writeValueAsString(m.getNumberOfProducts()));
+        Files.writeString(Paths.get("target/bestSellingProductsExclSubcats.json"), mapper.writeValueAsString(m.getBestSellingProducts(false)));
+        Files.writeString(Paths.get("target/bestSellingProductsInclSubcats.json"), mapper.writeValueAsString(m.getBestSellingProducts(true)));
+        Files.writeString(Paths.get("target/swedishProductsExclSubcats.json"), mapper.writeValueAsString(convertSePercent(m, false)));
+        Files.writeString(Paths.get("target/swedishProductsInclSubcats.json"), mapper.writeValueAsString(convertSePercent(m, true)));
         System.out.println("Stuff done");
+    }
+
+    private List<NameValuePair> convertSePercent(MatCategory m, boolean inclSubCategories) {
+        List<NameValuePair> nvp = new ArrayList<>();
+        Map<String, MatCategory.Count> seCounts = m.getSwedishProducts(inclSubCategories);
+        for (String key : seCounts.keySet()) {
+            MatCategory.Count count = seCounts.get(key);
+            nvp.add(new NameValuePair(key, (count.getPartCount() * 100) / count.getTotCount()));
+        }
+        return nvp;
+    }
+
+    @Data
+    public class NameValuePair {
+        private String product;
+        private long value;
+
+        public NameValuePair(String product, long value) {
+            this.product = product;
+            this.value = value;
+        }
     }
 }
--- src/main/java/info/henrysson/advent_bonus/model/MatCategory.java
+++ src/main/java/info/henrysson/advent_bonus/model/MatCategory.java
@@ -1,21 +1,100 @@
 package info.henrysson.advent_bonus.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import info.henrysson.advent_bonus.service.MatProductService;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.lang.Math.min;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MatCategory {
-    @Autowired
-    private static MatProductService matProductService;
 
     private long id;
     private String name;
     private List<MatCategory> subCategories = new ArrayList<>();
     private List<MatProduct> products;
+
+    public Map<String, Integer> getNumberOfProducts() {
+        Map<String, Integer> prodCount = new HashMap<>();
+        prodCount.put(name, products.size());
+        for (MatCategory mc : subCategories) {
+            prodCount.putAll(mc.getNumberOfProducts());
+        }
+        return prodCount;
+    }
+
+    // Använd en databas nästa gång...
+    // Vad är bestselling? Antal, antal * pris, antal * (pris - cost)?
+    // Varför blir det samma oavsett inclSubCategories????
+    public Map<String, Map<String, Long>> getBestSellingProducts(boolean inclSubCategories) {
+        Map<String, Map<String, Long>> topProducts = new HashMap<>();
+        for (MatCategory mc : subCategories) {
+            topProducts.putAll(mc.getBestSellingProducts(inclSubCategories));
+        }
+        Map<String, Long> categoryProducts = new HashMap<>();
+        for (MatProduct mp : products) {
+            categoryProducts.put(mp.getName(), mp.getSoldCount());
+        }
+        if (inclSubCategories) {
+            for (String key : topProducts.keySet()) {
+                categoryProducts.putAll(topProducts.get(key));
+            }
+        }
+        List<NameValuePair> viableProducts = new ArrayList<>();
+        for (String key : categoryProducts.keySet()) {
+            viableProducts.add(new NameValuePair(key, categoryProducts.get(key)));
+        }
+        List<NameValuePair> top5prods = viableProducts.stream().sorted(new Comparator<NameValuePair>() {
+            @Override
+            public int compare(NameValuePair o1, NameValuePair o2) {
+                return Long.valueOf(o2.getValue()).compareTo(o1.getValue());  // Reverse order wanted
+            }
+        }).collect(Collectors.toList()).subList(0, min(5, viableProducts.size()));
+        Map<String, Long> top5Map = new HashMap<>();
+        for (NameValuePair nvp : top5prods) {
+            top5Map.put(nvp.getProduct(), nvp.getValue());
+        }
+        topProducts.put(name, top5Map);
+        return topProducts;
+    }
+
+    public Map<String, Count> getSwedishProducts(boolean inclSubCategories) {
+        long partCount = products.stream().filter(p -> "SE".equals(p.getCountryOfOrigin())).count();
+        long totCount = products.size();
+        Map<String, Count> seCounts = new HashMap<String, Count>();
+        for (MatCategory mc : subCategories) {
+            seCounts.putAll(mc.getSwedishProducts(inclSubCategories));
+        }
+        partCount += inclSubCategories ? seCounts.values().stream().map(Count::getPartCount).reduce(0L, Long::sum) : 0;
+        totCount += inclSubCategories ? seCounts.values().stream().map(Count::getTotCount).reduce(0L, Long::sum) : 0;
+        seCounts.put(name, new Count(totCount, partCount));
+        return seCounts;
+    }
+
+    @Data
+    public class NameValuePair {
+        private String product;
+        private long value;
+
+        public NameValuePair(String product, long value) {
+            this.product = product;
+            this.value = value;
+        }
+    }
+
+    public class Count {
+        @Getter
+        long totCount;
+        @Getter
+        long partCount;
+
+        public Count(long totCount, long partCount) {
+            this.totCount = totCount;
+            this.partCount = partCount;
+        }
+    }
 }
